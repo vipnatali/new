04acbc499942d2f72c833eea16bf2778afe2f574
Natali <vipnatalii@bk.ru>
2022-11-10 14:05:25 +0500

    решение первой задачи

---
+++ 1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Составить программу вывода на эран в одну строку четырех любых чисел с тремя пробелами между ними.
+int a = new Random ().Next (45, 900);
+int b = new Random ().Next (45, 900);
+int c = new Random ().Next (45, 900);
+int d = new Random ().Next (45, 900);
+
+Console.WriteLine ($"Решение задачи: {a} {b} {c} {d}");
